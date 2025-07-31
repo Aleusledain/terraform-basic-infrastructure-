228ce072806fd0bd587d8b6e5ec5bf8aeb486782
McIntyre <150527506+Aleusledain@users.noreply.github.com>
2025-07-31 08:28:37 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,13 @@
 provider "aws" {
-  region = us-east-1
+  region = "us-east-1"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-}
-
-resource "aws_security_group" "web_sg" {
-  name        = "web_sg"
-  description = "Allow SSH"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "web" {
-  ami           = var.ami_id
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "my-unique-first-terraform-bucket-12345"
+  acl    = "private"
 
   tags = {
-    Name = "BasicEC2"
+    Name        = "MyFirstBucket"
+    Environment = "Dev"
   }
 }
