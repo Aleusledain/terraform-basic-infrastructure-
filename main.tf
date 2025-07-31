299bc071cc19bf4e6e6b7fa7beb362fb725ce8ef
McIntyre <150527506+Aleusledain@users.noreply.github.com>
2025-07-31 08:37:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "my-unique-first-terraform-bucket-12345"
+  bucket = "a-unique-terraform-bucket4563"
   acl    = "private"
 
   tags = {
